940f78762c61d3c9f187a288bd6c4928f3ee6fad
Ariful0007 <arifulpub143@gmail.com>
2023-09-28 14:15:16 +0600

    Initial Commit

---
+++ swiftSdkforgrozziieUITests/swiftSdkforgrozziieUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swiftSdkforgrozziieUITestsLaunchTests.swift
+//  swiftSdkforgrozziieUITests
+//
+//  Created by sang on 28/9/23.
+//
+
+import XCTest
+
+class swiftSdkforgrozziieUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
